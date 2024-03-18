fc8626b94b952e6ca53dca6ea97e2e5e89373279
jectgenius <jkl9087@naver.com>
2024-03-18 11:08:26 +0900

    Feat: 1장 시작하기

---
+++ SimJaeram/_01_start/src/main/java/com/sh/example/Sample1.java
@@ -0,0 +1,7 @@
+package com.sh.example;                                        // 패키지 선언문, Sample1 클래스 경로명
+
+class Sample1 {                                                // Sample1 클래스 정의 시작
+    public static void main(String[] args) {                   // main() 메소드 정의 시작
+        System.out.println("Java 세상에 오신 것을 환영합니다!");    // println() 메소드 호출하여 "Java 세상에 오신 것을 환영합니다!" 문자열 출력
+    }                                                          // main() 메소드 정의 끝
+}                                                              // Sample 클래스 정의 끝
---
+++ SimJaeram/_01_start/src/main/java/com/sh/exercise/Sample2.java
@@ -0,0 +1,7 @@
+package com.sh.exercise;
+
+class Sample2 { // 클래스 이름 변경은 refactor 이용한다.
+    public static void main(String[] args) {
+        System.out.println("Java 세상에 오신 것을 환영합니다!");
+    }
+}
