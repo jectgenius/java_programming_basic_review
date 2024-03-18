736b29c8af8c5ce494a87e2978e1fb0af58c2f9f
jectgenius <jkl9087@naver.com>
2024-03-18 16:00:47 +0900

    Feat: 3장 변수

---
+++ SimJaeram/_03_variable/src/main/java/com/sh/exercise/SampleP2.java
@@ -0,0 +1,10 @@
+package com.sh.exercise;
+
+// 수정 후 SampleP2
+class SampleP2 {                                                // SampleP2Before 클래스 정의 시작
+    public static void main(String[] args) {                    // main() 메소드 정의 시작
+        double num;                                             // double형 변수 num 선언
+        num = 3.14;                                             // double형 변수 num에 3.14 대입연산
+        System.out.println("변수 num의 값은 " + num + "입니다.");  // System.out.println() 메소드 호출하여 변수 num에 저장된 값 출력
+    }                                                           // main() 메소드 정의 끝
+}                                                               // SampleP2Before 클래스 정의 시작
---
+++ SimJaeram/_03_variable/src/main/java/com/sh/exercise/SampleP2Before.java
@@ -0,0 +1,10 @@
+package com.sh.exercise;
+
+// 수정 전 SampleP2
+class SampleP2Before {
+    public static void main(String[] args) {
+        char ch;
+//        ch = 3.14;                                              // char형 변수 ch에 실수 3.14 대입연산하여 컴파일 오류
+//        System.out.println("변수 ch의 값은 " + ch + "입니다.");
+    }
+}
